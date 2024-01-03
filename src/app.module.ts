ea9c26ca344f9bf55d251553bd8381a9b5c175c1
Natan <zerogamerptbr@gmail.com>
2024-01-03 14:16:16 -0300

    :lock: security: config module using in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { MailerService } from './mailer/mailer.service';
+import { MailerModule } from './mailer/mailer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    MailerModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService, MailerService],
 })
 export class AppModule {}
