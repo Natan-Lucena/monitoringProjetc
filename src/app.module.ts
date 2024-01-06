82607087ab94bb58668da64c4e5e5048a9573578
Natan <zerogamerptbr@gmail.com>
2024-01-06 14:10:04 -0300

    :recycle: refac: app module is now working with the others modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { MailerService } from './mailer/mailer.service';
 import { MailerModule } from './mailer/mailer.module';
 import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
-import { MonitoriaService } from './monitoria/monitoria.service';
 import { MonitoriaModule } from './monitoria/monitoria.module';
 
 @Module({
@@ -20,7 +13,5 @@ import { MonitoriaModule } from './monitoria/monitoria.module';
     MailerModule,
     MonitoriaModule,
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService, MailerService, JwtService, MonitoriaService],
 })
 export class AppModule {}
