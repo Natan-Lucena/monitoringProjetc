210b4e13a5c0ed3f08686109ecac76b66d689a28
Natan <zerogamerptbr@gmail.com>
2024-01-11 13:28:22 -0300

    :sparkles: feat: add mailer service at monitoria module

--- src/monitoria/monitoria.module.ts
+++ src/monitoria/monitoria.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MonitoriaController } from './monitoria.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MonitoriaService } from './monitoria.service';
+import { MailerService } from 'src/mailer/mailer.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [MonitoriaController],
-  providers: [MonitoriaService],
+  providers: [MonitoriaService, MailerService],
 })
 export class MonitoriaModule {}
