8cec7b94ee5999728eee5bfa9f7405a4ca1bbfcf
Natan <zerogamerptbr@gmail.com>
2024-01-13 02:03:44 -0300

    :recycle: refac: add the password at the dto

--- src/auth/dtos/forgetPassword.dto.ts
+++ src/auth/dtos/forgetPassword.dto.ts
@@ -1,6 +1,10 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class ForgetPasswordDTO {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
+  @IsString()
+  @IsOptional()
+  password?: string;
 }
