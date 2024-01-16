101b5e92e8f94fdf32756257e016e129278b51bb
Natan <zerogamerptbr@gmail.com>
2024-01-16 00:46:25 -0300

    :recycle: refac: app module with the new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { MailerModule } from './mailer/mailer.module';
 import { ConfigModule } from '@nestjs/config';
 import { MonitoriaModule } from './monitoria/monitoria.module';
 import { UserModule } from './user/user.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { InfraService } from './infra/infra.service';
+import { InfraModule } from './infra/infra.module';
+import { DateProviderService } from './date-provider/date-provider.service';
+import { DateProviderModule } from './date-provider/date-provider.module';
 
 @Module({
   imports: [
@@ -14,6 +19,10 @@ import { UserModule } from './user/user.module';
     MailerModule,
     MonitoriaModule,
     UserModule,
+    ScheduleModule.forRoot(),
+    InfraModule,
+    DateProviderModule,
   ],
+  providers: [InfraService, DateProviderService],
 })
 export class AppModule {}
