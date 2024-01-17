849cb4f523ca59107ce7c3b492343df44afffa68
Natan <zerogamerptbr@gmail.com>
2024-01-17 00:28:13 -0300

    :sparkles: feat: monitoria module now have the correct providers

--- src/monitoria/monitoria.module.ts
+++ src/monitoria/monitoria.module.ts
@@ -3,10 +3,11 @@ import { MonitoriaController } from './monitoria.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { MonitoriaService } from './monitoria.service';
 import { MailerService } from 'src/mailer/mailer.service';
+import { DateProviderService } from 'src/date-provider/date-provider.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [MonitoriaController],
-  providers: [MonitoriaService, MailerService],
+  providers: [MonitoriaService, MailerService, DateProviderService],
 })
 export class MonitoriaModule {}
