d14d8e5b1733b1e38878966f0e43cb962014b96e
Natan Lucena <zerogamerptbr@gmail.com>
2024-05-02 15:34:33 -0300

    build: change swagger route

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Documentação com Swagger - Monitoring Projetc')
-  .setDescription(
-    'O Swagger, também conhecido como OpenAPI, é uma biblioteca amplamente utilizada no contexto backend, disponível para várias linguagens e frameworks. Ele fornece uma interface que descreve detalhadamente cada endpoint e estrutura de dados da sua aplicação. Neste contexto universitário, o projeto de gerenciamento de monitorias é essencial para organizar e acompanhar as atividades de monitoria em uma universidade. Esta documentação detalha os endpoints e estruturas de dados relacionados ao gerenciamento de monitorias de cadeiras, permitindo uma melhor compreensão e integração com a aplicação.',
-  )
-  .setVersion('1.0')
-  .addTag('monitoria')
-  .addTag('auth')
-  .addTag('user')
-  .build();
+    .setTitle('Documentação com Swagger - Monitoring Project')
+    .setDescription(
+      'O Swagger, também conhecido como OpenAPI, é uma biblioteca amplamente utilizada no contexto backend, disponível para várias linguagens e frameworks. Ele fornece uma interface que descreve detalhadamente cada endpoint e estrutura de dados da sua aplicação. Neste contexto universitário, o projeto de gerenciamento de monitorias é essencial para organizar e acompanhar as atividades de monitoria em uma universidade. Esta documentação detalha os endpoints e estruturas de dados relacionados ao gerenciamento de monitorias de cadeiras, permitindo uma melhor compreensão e integração com a aplicação.',
+    )
+    .setVersion('1.0')
+    .addTag('monitoria')
+    .addServer('http://localhost:3333/', 'Local environment')
+    .addTag('auth')
+    .addTag('user')
+    .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
