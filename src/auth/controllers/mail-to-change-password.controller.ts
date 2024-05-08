ed5212cd9eedb9d57c58604e7aa35f2e3f2f33f4
Zer0Rx <102997123+Natan-Lucena@users.noreply.github.com>
2024-05-08 08:20:44 -0300

    Develop (#15)
    * feat: add auth token in validation
    
    * Docs: add Swagger setup and Auth module docs (#14)
    
    ---------
    
    Co-authored-by: André <93006884+Andremelo001@users.noreply.github.com>

--- src/auth/controllers/mail-to-change-password.controller.ts
+++ src/auth/controllers/mail-to-change-password.controller.ts
@@ -4,19 +4,35 @@ import {
     HttpCode,
     HttpStatus,
     Post,
+    UseGuards,
   } from '@nestjs/common';
-  import { MailToChangePasswordService } from '../services/mail-to-change-password.service';
-  import { ForgetPasswordDTO } from '../dtos';
-import { ApiTags } from '@nestjs/swagger';
+import { MailToChangePasswordService } from '../services/mail-to-change-password.service';
+import { ForgetPasswordDTO } from '../dtos';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from '../guard';
   
-  @ApiTags('auth')
-  @Controller('auth')
-  export class MailToChangePasswordController {
-    constructor(private mailToChangePasswordService: MailToChangePasswordService) {}
+@ApiTags('auth')
+@Controller('auth')
+@UseGuards(JwtGuard)
+export class MailToChangePasswordController {
+  constructor(private mailToChangePasswordService: MailToChangePasswordService) {}
   
-    @HttpCode(HttpStatus.OK)
-    @Post('forgetPassword')
-    mailToChangePassword(@Body() dto: ForgetPasswordDTO) {
+  @HttpCode(HttpStatus.OK)
+  @Post('forgetPassword')
+  @ApiOperation({ summary: 'Send password change email' })
+  @ApiBody({
+    type: ForgetPasswordDTO,
+    description: 'Data required to send password change email',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Password change email sent successfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Unauthorized: Invalid or expired authentication token',
+  })
+  mailToChangePassword(@Body() dto: ForgetPasswordDTO) {
       return this.mailToChangePasswordService.mailToChangePassword(dto);
-    }
+  }
 }
--- src/auth/dtos/createUser.dto.ts
+++ src/auth/dtos/createUser.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsString,
@@ -9,20 +10,56 @@ import {
 export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'João da Silva',
+    description: `O nome do usuário`,
+    required: true,
+    type: 'string',
+  })
   name: string;
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'exemplo123@gmail.com',
+    description: `O endereço de e-mail do usuário.`,
+    required: true,
+    type: 'string',
+  })
   email: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'Exemplo123@',
+    description: `A senha do usuário.`,
+    required: true,
+    type: 'string',
+  })
   password: string;
   @IsBoolean()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'sim',
+    description: `Indica se o usuário é um monitor.`,
+    required: true,
+    type: 'boolean',
+  })
   isMonitor: boolean;
   @IsBoolean()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'não',
+    description: `Indica se o usuário é um professor.`,
+    required: true,
+    type: 'boolean',
+  })
   isProfessor: boolean;
   @IsString()
   @IsOptional()
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    description: `O token de autenticação do usuário, se aplicável.`,
+    required: false,
+    type: 'string',
+  })
   token: string;
 }
